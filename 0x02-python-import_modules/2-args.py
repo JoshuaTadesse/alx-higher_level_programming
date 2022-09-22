8d13bd114263c6650f1a725cc554706511c29b0b
JoshuaT.Mekuriaw <eshewtadesse@gmail.com>
2022-09-22 14:09:39 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     argv1 = sys.argv
     largv = len(argv1)
-    if(largv == 2):
-        print("{} argument:".format(largv - 1))
-    elif(largv == 1):
+    if(largv == 1):
         print("{} arguments.".format(largv - 1))
+    elif(largv == 2):
+        print("{} argument:".format(largv - 1))
         print("{}: {}".format(largv, argv1[0]))
     else:
         print("{} arguments:".format(largv - 1))
